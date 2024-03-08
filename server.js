25666de154cd6255df15665e10c957deef719b25
tahira-sakhi <tahi.parveen93@gmail.com>
2024-03-08 13:37:13 +0500

    added staging

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-const express = require ("express");
-const app = express ();
+const express = require("express");
+const app = express();
 const port = 5000;
 
-
-
-
-app.get("/",(req,res)=>{
-    res.send("Helo this is my first github repository!")
+app.get("/", (req, res) => {
+  res.send("Hello this is my first github repository!");
 });
 
-
-app.listen (port, (err, data)=> {
-    if (err){
-        console.log(err)
-    }
-    else {
-        console.log("http://localhost:5000/")}
-    });
+app.listen(port, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("http://localhost:5000/");
+  }
+});
